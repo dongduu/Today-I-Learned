58c2bd8755402c7a005884057a68b1dd86276ca8
dongduu <4012popo@naver.com>
2022-03-04 00:00:38 +0900

    feat: practice axios & chart.js

---
+++ react-axios/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import AxiosApi from "./AxiosApi";
+
+function App() {
+  return (
+    <div className="App">
+      <div>안녕하세요</div>
+
+      <AxiosApi />
+    </div>
+  );
+}
+
+export default App;
